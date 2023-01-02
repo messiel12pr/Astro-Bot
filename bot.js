fd6c99c26e53e82a6e7bc8f6d13266dd4821022a
Joel <95717805+messiel12pr@users.noreply.github.com>
2023-01-01 23:42:04 -0400

    added a request for Mars Rover Photos api
    added a new command called rover and it requires a parameter which is the date of the photo

--- bot.js
+++ bot.js
@@ -6,9 +6,9 @@ const { EmbedBuilder } = require('discord.js');
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
-
+  
   if (interaction.commandName === 'apod') {
-    // api request
+    // api request for astronomy picture of the day
     const request = require('request');
     request('https://api.nasa.gov/planetary/apod?api_key=cQDCapa6pSqUPx6ignf3p2ImqC7t7Lx3n26H5X9M', { json: true }, (err, res, body) => {
       if (err) { return console.log(err); }
@@ -21,6 +21,21 @@ client.on('interactionCreate', async interaction => {
 	    .setImage(body.url);
 
       interaction.channel.send({ embeds: [apodEmbed]});
+    });
+  }
+
+  else if(interaction.commandName === 'rover') {
+    // api request for a picture taken by the mars rover on a given date
+    const request = require('request');
+    request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=cQDCapa6pSqUPx6ignf3p2ImqC7t7Lx3n26H5X9M&earth_date='+interaction.options.getString('date'), {json: true}, (err, res, body) => {
+      if(err) {return console.log(err); }
+
+      const roverEmbed = new EmbedBuilder()
+      .setColor(0x5D3FD3)
+      .setTitle('Photo taken by Mars '+ body.photos[0].rover.name + ' Rover on ' + interaction.options.getString('date'))
+	    .setImage(body.photos[0].img_src);
+
+      interaction.channel.send({ embeds: [roverEmbed]});
     });
   }
 });
